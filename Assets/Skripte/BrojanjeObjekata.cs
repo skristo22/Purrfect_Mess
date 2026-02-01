dd7474571e28736a03b1892c34f3c761e265a0b2
skristo22 <skristo22@student.foi.hr>
2026-02-01 21:58:40 +0100

    Dodana logika rušenja objekata, objekti za rušenje i brojač

---
+++ Assets/Skripte/BrojanjeObjekata.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BrojanjeObjekata : MonoBehaviour
+{
+    public static BrojanjeObjekata Instance;
+
+    public int objectsToDestroy = 5;
+    public int destroyedCount = 0;
+
+    public TextMeshProUGUI counterText;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    public void ObjectHitGround()
+    {
+        destroyedCount++;
+        UpdateUI();
+
+        if (destroyedCount >= objectsToDestroy)
+        {
+            WinGame();
+        }
+    }
+
+    void UpdateUI()
+    {
+        if (counterText != null)
+            counterText.text = $"Sruseno: {destroyedCount} / {objectsToDestroy}";
+    }
+
+    void WinGame()
+    {
+        Debug.Log("🎉 POBJEDA! Sve si srušila!");
+        // ovdje kasnije ide end screen
+    }
+}
---
+++ Assets/Skripte/RusenjeObjekata.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RusenjeObjekata : MonoBehaviour
+{
+    private bool alreadyCounted = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (alreadyCounted) return;
+
+        if (collision.collider.CompareTag("Pod"))
+        {
+            alreadyCounted = true;
+
+            BrojanjeObjekata.Instance.ObjectHitGround();
+
+            // opcionalno: zvuk / razbijanje / animacija
+        }
+    }
+}
